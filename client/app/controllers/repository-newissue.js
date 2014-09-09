f07603379f3f1e51c91aab95d3c1d55e307720f9
gomenasaai <contramundee@gmail.com>
2014-09-09 10:02:05 +0100

    new feature: adding new issue

---
+++ client/app/controllers/repository-newissue.js
@@ -0,0 +1,28 @@
+import Ember from 'ember';
+
+export default Ember.Controller.extend({
+	needs: ["repository"],
+  repo: Ember.computed.alias("controllers.repository"),
+  issue : function(){
+      return Github.Issue.create();
+  }.property("repo.model"),
+  actions: {
+    submitIssue: function () {
+      //var vals = this.getProperties("title", "body");
+      //console.log(vals);
+      //POST issues_url
+
+      var issue = this.get("issue");
+      var url = this.get("repo").get("issues_url");
+      //Ember.$.post(url, { title: title, body: body }, function (result) {
+      //    //success...
+      //    this.transitionToRoute("issues");
+      //    alert("Issue submitted");
+      //});
+      console.log("Submitted " + issue.get("title") + " to " + url);
+      //reset it
+      this.set("issue", Github.Issue.create());
+      this.transitionToRoute("issues");
+    }
+  }
+});
